7b6e241d87f651d1818353b67801415dfddc94c8
Mqasim0 <muhammadqasim3991@gmail.com>
2023-11-07 15:25:49 +0500

    initialize project install dependences & create user schema

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+const 
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageUrl: {
+      type: String,
+      default: "/images/default.png",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
