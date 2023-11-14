d95ba6a2fe7d80b79de3c8a03119d98708ec9123
Mqasim0 <muhammadqasim3991@gmail.com>
2023-11-14 17:50:59 +0500

    add comment end point or blogById

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const multer = require("multer");
 const path = require("path");
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 
 const router = Router();
 
@@ -21,6 +22,27 @@ router.get("/add-new", (req, res) => {
   return res.render("addBlog", {
     user: req.user,
   });
+});
+
+router.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id).populate("createdBy");
+
+  console.log("blog ===>", blog);
+
+  return res.render("blog", {
+    user: req.user,
+    blog: blog,
+  });
+});
+
+router.post("/comment/:blogId", async (req, res) => {
+  await Comment.create({
+    content: res.body.content,
+    blogId: req.params.blogId,
+    createdBy: req.user._id,
+  });
+
+  return res.redirect(`/blog/${req.params.blogId}`);
 });
 
 router.post("/", upload.single("coverImage"), async (req, res) => {
